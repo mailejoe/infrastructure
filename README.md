e581d507d64c1ffc7ffcae3945dbfc029578ec08
Marc Fisher <mcfisher83@gmail.com>
2021-11-12 11:27:42 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mailejoe Infrastructure
+
+Provides the cloudformation templates for setting up Mailejoe from scratch including
+creating a new VPC for it to run in.
+
+## Features
+
+- Alerts pushed to Discord with a fallback email option
+- New 3AZ VPC setup for the ECS cluster to run in
+- Best practice security setup including flowlogs, logging, cloudwatch alerts, and more.
---
+++ lambda/discord.js
@@ -0,0 +1,76 @@
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from '@aws-sdk/client-secrets-manager';
+import * as axios from 'axios';
+
+const retrieveSecrets = async () => {
+  const client = new SecretsManagerClient({ region: process.env.REGION });
+  const command = new GetSecretValueCommand({ SecretId: process.env.SECRET_ID });
+  const response = await client.send(command);
+
+  return JSON.parse(response.SecretString);
+};
+
+const getMsgInfo = (event) => {
+  return [
+    {
+      name: 'service',
+      value: event['Trigger']['Dimensions'][0].value,
+      inline: true
+    },
+    {
+      name: 'alarm',
+      value: event['AlarmName'],
+      inline: true
+    },
+    {
+      name: 'description',
+      value: event['AlarmDescription'],
+      inline: true
+    },
+    {
+      name: 'oldestState',
+      value: event['OldStateValue'],
+      inline: true
+    },
+    {
+      name: 'trigger',
+      value: event['Trigger']['MetricName'],
+      inline: true
+    },
+    {
+      name: 'event',
+      value: event['NewStateReason'],
+      inline: true
+    }
+  ];
+}
+
+module.exports.handler = async function handler(event) {
+  let config = await retrieveSecrets();
+
+  event.Records.forEach((record) => {
+    const msg = record['Sns']['Message'];
+    if (msg.Trigger) {
+      const discordData = {
+        content: '',
+        embeds: [{
+          color: 15158332,
+          fields: getMsgInfo(record),
+        }],
+      };
+
+      try {
+        await axios.post(config.DISCORD_WEBHOOK, discordData);
+        console.log(`Discord Response: ${response.status} ${response.statusText}`);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  });
+
+  //https://discord.com/api/webhooks/904759385291620352/ebieJ6j0prydboriKE7Kph43PxmS7TXWOn4u8llrcSzsApbwLTx-u2N1Aadb0BjB-jrd
+
+  return 'SNS -> Discord completed successfully';
+};
